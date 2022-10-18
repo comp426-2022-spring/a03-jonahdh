4c88fb096090be80785b2a5c6664add7e771a917
jonahdh <jonahdh@email.unc.edu>
2022-10-18 18:10:00 -0400

    Added "check endpoint"

--- server.js
+++ server.js
@@ -6,19 +6,23 @@ const args = minimist(process.argv.slice(2));
 
 args['port'];
 
-const app = express();
 const HTTP_PORT = args.port || 3000;
+const app = express();
 
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+});
 
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
-
