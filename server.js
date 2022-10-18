a98856bc8fd75a7eb7ab5183cae76a3cc76e5ab8
jonahdh <jonahdh@email.unc.edu>
2022-10-18 17:42:52 -0400

    Basic express server start

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import minimist from 'minimist';
+import {flip, flipACoin, coinFlip} from 'modules/coin.mjs';
+
+const app = express();
+const port = 3000
+
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
